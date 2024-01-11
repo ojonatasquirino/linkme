2717bbf2420dc5627d16e551928950eaa4109948
Jônatas Quirino <quirinoj02@gmail.com>
2024-01-11 01:23:58 -0300

    automatizando ano

--- assets/js/global/global.js
+++ assets/js/global/global.js
@@ -1,12 +1,23 @@
-(function (){
-
+(function () {
   // Bloqueando arrastar imagens do site para baixar
 
-  function desativarDragDrop () {
-    document.addEventListener('dragstart', (evento) => evento.preventDefault(), false)
-    document.addEventListener('drop', (evento) => evento.preventDefault(), false)
+  function desativarDragDrop() {
+    document.addEventListener(
+      "dragstart",
+      (evento) => evento.preventDefault(),
+      false
+    );
+    document.addEventListener(
+      "drop",
+      (evento) => evento.preventDefault(),
+      false
+    );
   }
 
-  desativarDragDrop()
+  desativarDragDrop();
+})();
 
-}())
+// Atualizando o ano footer automaticamente
+
+const year = document.querySelector("#current-year");
+year.innerHTML = new Date().getFullYear();
